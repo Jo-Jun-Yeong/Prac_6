8c846bd027bc10f78b544ce450c0547c05b944ad
test1 <jojun-yeong@218.38.137.28>
2021-08-13 19:59:59 +0900

    08/13 save

---
+++ src/Ex6.java
@@ -0,0 +1,21 @@
+//두 점의 거리를 계산하는 getDistance()를 완성하시오. 
+//[Hint] 제곱근 계산은 Math.sqrt(double a)를 사용하면 된다.
+//public class Ex6 {
+//
+//	public static void main(String[] args) {
+//		MyPoint get = new MyPoint();
+//		System.out.println(get.getDistance(0, 0, 0, 0));
+//	}
+//	
+//	
+//}
+//class MyPoint{
+//	static double getDistance(int x, int y, int x1, int y1) {
+//		double result = Math.sqrt(((x1-x)*(x1-x)+(y1-y)*(y1-y)));
+//				
+//		return result; 
+//	}
+//}
+
+//문제6-6에서 작성한 클래스메서드 getDistance()를 
+//MyPoint클래스의 인스턴스메서 드로 정의하시오.
---
+++ src/Ex7.java
@@ -0,0 +1,40 @@
+////문제6-6에서 작성한 클래스메서드 getDistance()를 
+////MyPoint클래스의 인스턴스메서드로 정의하시오.
+//
+// class Mypoint{
+//	int x;
+//	int y;
+//	
+//	
+//	Mypoint(int x, int y){
+//		this.x=x;
+//		this.y=y;
+//	}
+//	
+//	//
+//	
+//	double getDistance(int x, int y) {
+//		
+//		
+//		double result = Math.sqrt(
+//				((x-this.x)*(x-this.x))
+//							+((y-this.y)*(y-this.y))
+//								);
+////		double result = x;
+//		return result; 
+//	}
+//	
+//	
+//}
+//
+//public class Ex7 {
+//
+//	public static void main(String[] args) {
+//		 Mypoint p = new Mypoint(1, 1);
+//
+//		 
+////		 p와 (2,2) 사이의 거리를 구한다.
+//		 System.out.println(p.getDistance(2,2));
+//	}
+//
+//}
